9cf768bae2804052808c88eeece14b38863d03de
Vicctorri <brinza.misha1@gmail.com>
2021-12-10 14:10:44 +0200

    Add routes, view and controller

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+
+    public function showContact() {
+        return view('contact');
+    }
+
+    public function sendContact() {
+        return redirect()->route('showContact');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Route::get('/contact', function () {
+    return view('contact');
+});*/
+
+//Route::get('/contact', 'ContactController@showContact');
+
+
+Route::get('/contact', [ContactController::class, 'showContact'])->name('showContact');
+
