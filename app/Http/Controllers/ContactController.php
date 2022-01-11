ad4f7d66dc47005a6b3384f8c21763daeac9c333
Vicctorri <brinza.misha1@gmail.com>
2022-01-11 13:46:48 +0200

    Email sending for Contact form

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,12 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Request\ContactRequest;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Mail\Message;
 
 class ContactController extends Controller
 {
     public function index(){
         return view('home.contact');
+    }
+
+    public function send(ContactRequest $request):RedirectResponse
+    {
+        $callback = function (string $test): string{
+            return "{$test} input";
+        };
+
+
+        $data = $request->validated();
+        \Log::debug( 'test', $data);
+
+        \Mail::send('emails.contact',
+            [
+                'firstname' => $data['firstname'],
+                'lastname' => $data['lastname'],
+                'email' => $data['email'],
+                'phonenumber' => $data['phonenumber'],
+                'subject' => $data['subject'],
+                'messageText' => $data['message'],
+            ],
+        
+        function(Message $message) use ($data){ 
+            $message->subject('mesage from ' . $data['email']);
+            $message->to('tech@baloon.app');
+            $message->from('no-reply@baloon.app', 'Balon M');
+        
+
+        });
+
+
+        return redirect()->route('contact')->withInput($data);
     }
 }
 
--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -9,34 +9,37 @@
           </div>
           <div class="row">
               <div class="col-md-7 shadow rounded p-5 bg-light">
+                  <form action="{{ route('contact.send')}}" method="POST" name="contact-form">
                   <div class="row">
                       <div class="col mb-3">
                           <label class="form-label" >First Name</label>
-                          <input type="text" class="form-control" placeholder="First Name">
+                          <input type="text" value="{{ old('firstname') }}" class="form-control" placeholder="First Name" name="firstname">
                       </div>
                       <div class="col mb-3">
                           <label class="form-label" >Last Name</label>
-                          <input type="text" class="form-control" placeholder="Last Name">
+                          <input type="text" value="{{ old('lastname') }}" class="form-control" placeholder="Last Name" name="lastname">
                       </div>
                   </div>
                   <div class="mb-3">
                           <label class="form-label" >Email</label>
-                          <input type="text" class="form-control" placeholder="name@gmail.com">
+                          <input type="email" value="{{ old('email') }}" class="form-control" placeholder="name@gmail.com" name="email">
                   </div>
                   <div class="mb-3">
                       <label class="form-label" >Phone Number</label>
-                      <input type="text" class="form-control" placeholder="+37360000000">
+                      <input type="tel" value="{{ old('phonenumber') }}" class="form-control" placeholder="+37360000000" name="phonenumber">
                   </div>
                   <div class="mb-3">
                       <label class="form-label" >Subjet</label>
-                      <input type="text" class="form-control" placeholder="Subject">
+                      <input type="text" value="{{ old('subject') }}"  class="form-control" placeholder="Subject" name="subject">
                   </div>
                   <div class="mb-3">
                       <label class="form-label" >Message</label>
-                      <textarea type="text" class="form-control" placeholder="Write your message" rows="4"></textarea>
+                      <textarea type="text" class="form-control" placeholder="Write your message" rows="4" name="message">{{ old('message')}}</textarea>
                   </div>
-                  <button type="button" class="btn btn1 ">Submit</button>
+                  @csrf
+                  <button type="submit" class="btn btn1 ">Submit</button>
               </div>
+              </form>
 
               <div class="col-md-5">
                   <div class="ml-5">
