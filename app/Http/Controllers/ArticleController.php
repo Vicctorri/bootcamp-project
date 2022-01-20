fdf8b3c5e470cd891ff16417ec94e20e465191dd
Vicctorri <brinza.misha1@gmail.com>
2022-01-20 23:20:40 +0200

    Create LoggerInterface

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
+use Psr\Log\LoggerInterface;
 
 class ArticleController extends Controller
 {
-    public function show($articleId){
-        
+    public function show($articleId, Request $request, LoggerInterface $logger){
+
         $article = Article::findOrFail($articleId);
+
+        $user = $request->user();
+        $userRepresentation = $user ? "User with id {$user->id}" : "Unknown User";
+        $logger->info(
+            $userRepresentation . ' accessed ' . "article with id {$articleId}",
+            ['id' => $articleId, 'title' => $article->title],
+        );
+
         return view('article.article', ['article' => $article]);
     }
 }
