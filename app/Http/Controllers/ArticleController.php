bc5d3b14d82769875048a7b10b846ce0c36825f8
Vicctorri <brinza.misha1@gmail.com>
2022-01-27 13:50:51 +0200

    Changed ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,24 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
-use Psr\Log\LoggerInterface;
+use App\Services\ModelLogger;
 
 class ArticleController extends Controller
 {
-    public function show($articleId, Request $request, LoggerInterface $logger){
+    public function show($articleId, Request $request, ModelLogger $logger){
 
         $article = Article::findOrFail($articleId);
 
-        $user = $request->user();
-        $userRepresentation = $user ? "User with id {$user->id}" : "Unknown User";
-        $logger->info(
-            $userRepresentation . ' accessed ' . "article with id {$articleId}",
-                ['id' => $articleId,
-                'title' => $article->title,
-                'author_id'=>$article->author_id,
-                'seo_title'=>$article->seo_title,
-                'published_at'=>$article->published_at,
-                ]);
+        $logger->logModel($request->user(), $article);
 
         return view('article.article', ['article' => $article]);
     }
