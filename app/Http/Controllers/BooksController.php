ab1d694002a2da608155740d157a96b5e18a7177
Vicctorri <brinza.misha1@gmail.com>
2022-01-30 21:51:39 +0200

    Adjust BooksController for ModelLogger

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Services\ModelLogger;
 use Illuminate\Http\Request;
 
 class BooksController extends Controller
 {
-    public function index(){
-        return view('home.books');
+    /**
+     * @param $bookId
+     * @param Request $request
+     * @param ModelLogger $logger
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function index ($bookId, Request $request, ModelLogger $logger)
+    {
+        $book = Book::findOrFail($bookId);
+
+        $logger->logModel($request->user(),$book );
+        return view('home.books', ['book' => $book]);
     }
+
 }
