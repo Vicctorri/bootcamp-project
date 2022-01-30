0a1d98c15da714c40e8aa21385dba281b4489245
Vicctorri <brinza.misha1@gmail.com>
2022-01-30 21:43:01 +0200

    Changed route web

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::get('/blog/article/{id}', [ArticleController::class, 'show'])->name('blogArticle');
-Route::get('/books/{bookId}', [BooksController::class, 'index'])->name('books');
+Route::get('/books', [BooksController::class, 'index'])->name('books');
 Route::get('/books/book_page', [BookPageController::class, 'index'])->name('book_page');
 Route::get('/books/useful_advice', [UsefulAdviceController::class, 'index'])->name('useful_advice');
 Route::get('/books/cart', [CartController::class, 'index'])->name('cart');
